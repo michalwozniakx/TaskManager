9c4c205287db8c87726029bf2f333325c6e50f4f
michalwozniakx <michal.wozniak89a@gmail.com>
2021-12-04 11:39:00 +0100

    wczytanie danych z pliku task

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,5 +1,11 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class TaskManager {
 
     static final String FILE_NAME = "tasks.csv";
@@ -15,6 +21,32 @@ public class TaskManager {
     }
 
     public static void main(String[] args) {
+        tasks = loadDataToTab(FILE_NAME);
         printOptions(OPTIONS);
+    }
+
+    public static String[][] loadDataToTab(String fileName) {
+
+        Path dir = Paths.get(fileName);
+        if (!Files.exists(dir)) {
+            System.out.println("File not exist.");
+            System.exit(0);
+        }
+        String[][] tab = null;
+        try {
+            List<String> strings = Files.readAllLines(dir);
+            tab = new String[strings.size()][strings.get(0).split(",").length];
+
+            for (int i = 0; i < strings.size(); i++) {
+                String[] split = strings.get(i).split(",");
+                for (int j = 0; j < split.length; j++) {
+                    tab[i][j] = split[j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        return tab;
     }
 }
